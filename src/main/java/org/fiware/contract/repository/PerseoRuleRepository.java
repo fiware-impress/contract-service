dd96621d8689f96c12bdd1166b6fd3436d929bc9
Stefan Wiedemann <wistefan@googlemail.com>
2022-04-11 13:25:58 +0200

    automatically create sub

--- src/main/java/org/fiware/contract/repository/PerseoRuleRepository.java
+++ src/main/java/org/fiware/contract/repository/PerseoRuleRepository.java
@@ -1,6 +1,13 @@
 package org.fiware.contract.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.fiware.broker.api.SubscriptionsApiClient;
+import org.fiware.broker.model.EndpointReceiverInfoVO;
+import org.fiware.broker.model.EndpointVO;
+import org.fiware.broker.model.EntityInfoVO;
+import org.fiware.broker.model.EntityVO;
+import org.fiware.broker.model.NotificationParamsVO;
+import org.fiware.broker.model.SubscriptionVO;
 import org.fiware.contract.IdHelper;
 import org.fiware.contract.configuration.GeneralProperties;
 import org.fiware.perseo.api.RulesApiClient;
@@ -11,6 +18,8 @@ import org.fiware.perseo.model.RuleVO;
 
 import javax.inject.Singleton;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 @Singleton
 @RequiredArgsConstructor
@@ -19,8 +28,9 @@ public class PerseoRuleRepository {
 	private static final String RULE_NAME_TEMPLATE = "%s_rule";
 	private final GeneralProperties generalProperties;
 	private final RulesApiClient rulesApiClient;
+	private final SubscriptionsApiClient subscriptionsApiClient;
 
-	public void createRule(String orderId, String priceDefinitionId, String expressionText) {
+	public void createRule(String orderId, String priceDefinitionId, String expressionText, URI measuredEntity) {
 		HeadersVO headersVO = new HeadersVO();
 		headersVO.put("Content-Type", "application/json");
 		ActionParametersVO parametersVO = new ActionParametersVO()
@@ -38,6 +48,28 @@ public class PerseoRuleRepository {
 				.text(expressionText)
 				.action(postActionVO);
 		rulesApiClient.createRule(generalProperties.getTenant(), rule);
+		try {
+			createSubscription(measuredEntity);
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Was not able to create subscription", e);
+		}
+	}
+
+	private void createSubscription(URI measuredEntity) throws URISyntaxException {
+		EntityInfoVO entityInfoVO = new EntityInfoVO()
+				.id(measuredEntity);
+		NotificationParamsVO notificationParamsVO = new NotificationParamsVO()
+				.endpoint(new EndpointVO()
+						.uri(generalProperties.getPerseoUrl().toURI())
+						.addReceiverInfoItem(new EndpointReceiverInfoVO().key("Fiware-Service").value(generalProperties.getTenant()))
+				);
+		SubscriptionVO subscriptionVO = new SubscriptionVO()
+				.type(SubscriptionVO.Type.SUBSCRIPTION)
+				.entities(List.of(entityInfoVO))
+				.geoQ(null)
+				.isActive(true)
+				.notification(notificationParamsVO);
+		subscriptionsApiClient.createSubscription(subscriptionVO, generalProperties.getTenant());
 	}
 
 }
